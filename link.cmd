63b61ab5bd113efc443264bc3c6f107b6adb560c
ar <a.rau@comlog.org>
2017-12-01 16:13:06 +0100

    + Process monitor

---
+++ link.cmd
@@ -0,0 +1,3 @@
+npm link comlog-system-monitor-event-sipgate-sms comlog-system-monitor-process comlog-system-monitor-event-email comlog-system-monitor-filecount comlog-system-monitor-filetime comlog-system-monitor-ftp comlog-system-monitor-http
+cd ..\comlog-system-monitor-process
+npm link ..\comlog-process-manager
