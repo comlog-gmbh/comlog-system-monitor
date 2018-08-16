8dc84b81c6b9f4a4bb10cf232b9ad16f60409b0b
ar <a.rau@comlog.org>
2018-08-16 13:44:51 +0200

    ...

--- link.cmd
+++ link.cmd
@@ -1,3 +1,3 @@
-npm link ..\comlog-system-monitor-event-sipgate-sms ..\comlog-system-monitor-process ..\comlog-system-monitor-event-email ..\comlog-system-monitor-filecount ..\comlog-system-monitor-filetime ..\comlog-system-monitor-ftp ..\comlog-system-monitor-http ..\comlog-system-monitor-log
+npm link ..\comlog-system-monitor-event-sipgate-sms ..\comlog-system-monitor-process ..\comlog-system-monitor-event-email ..\comlog-system-monitor-filecount ..\comlog-system-monitor-filetime ..\comlog-system-monitor-ftp ..\comlog-system-monitor-sftp ..\comlog-system-monitor-http ..\comlog-system-monitor-log
 cd ..\comlog-system-monitor-process
 npm link ..\comlog-process-manager
